36ec1ac21fbf2422757576ca9da40597ffd5b0a2
Pedrohjc <pedrohj.carvalho@gmail.com>
2020-03-18 11:44:17 -0400

    Adding new ErrorDetail

--- src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -7,8 +7,10 @@ import br.com.devdojo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -39,7 +41,8 @@ public class StudentEndPoint {
     }
 
     @PostMapping
-    public ResponseEntity<?> save(@RequestBody Student std) {
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> save(@Valid @RequestBody Student std) {
         return new ResponseEntity<>(dao.save(std), HttpStatus.CREATED);
     }
 
