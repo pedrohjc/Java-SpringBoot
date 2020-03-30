fe752a5e44c884b9ec53075f9fc0e2c54be3893c
Pedrohjc <pedrohj.carvalho@gmail.com>
2020-03-30 12:36:39 -0400

    Adding Pageables and Adapters

---
+++ src/main/java/br/com/devdojo/adapter/SpringBootEssentialsAdapter.java
@@ -0,0 +1,22 @@
+package br.com.devdojo.adapter;
+
+
+import net.bytebuddy.build.Plugin;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class SpringBootEssentialsAdapter implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver phmar = new PageableHandlerMethodArgumentResolver();
+        phmar.setFallbackPageable(PageRequest.of(0,5));
+        resolvers.add(phmar);
+    }
+}
